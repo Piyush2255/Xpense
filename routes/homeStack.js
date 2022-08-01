d01c4a27d22a02154d769e0cc134c45b8ddd7ffb
Piyush2255 <piyush25girdhar@gmail.com>
2022-08-01 16:57:19 +0530

    Designed Home Screen

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,17 +1,35 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/home';
-import Detail from '../screens/detail';
+import Expense from '../screens/detail';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-const Stack=createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function Route() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={Home}/>
-        <Stack.Screen name='Detail' component={Detail}/>
-      </Stack.Navigator>
+      <Tab.Navigator
+        initialRouteName='Home'
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            }
+            else if (route.name === 'Expenses') {
+              iconName = focused ? 'cash' : 'cash-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color}></Ionicons>
+          },
+          tabBarActiveTintColor: '#e580ff',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='Expenses' component={Expense} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,200 @@
 import React from "react";
-import {StyleSheet,View,Text,Button} from "react-native";
+import { StyleSheet, View, Text, Button, Pressable, TouchableOpacity, FlatList, ScrollView } from "react-native";
 
-export default function Home({navigation}){
-    return(
-        <View>
-            <Text>Home Screen</Text>
-            <Button title="Go to Details" onPress={()=>navigation.navigate('Detail')}/>
+const DATA = [
+    {
+        id: "skjdfjekjfjasc",
+        title: "Grocery Shopping",
+        category: "Grocery",
+        amount: "1000"
+    },
+    {
+        id: "bvkreljfkwefvajdn",
+        title: "Chhole Bhature",
+        category: "Food",
+        amount: "100"
+    },
+    {
+        id: "kjvrjkjwenkjkjdvsdjz",
+        title: "Jeans",
+        category: "Clothing",
+        amount: "1500"
+    },
+    {
+        id: "jhdkfjafkjaejfnksdv",
+        title: "Petrol",
+        category: "Fuel",
+        amount: "500"
+    },
+    {
+        id: "jdkfjcdkjhcksjdckd",
+        title: "Gold Ring",
+        category: "Jewelry",
+        amount: "18000"
+    },
+];
+
+const Item = ({ item, index }) => {
+    return (
+        <View style={styles.expenseItem}>
+            <Text style={styles.expenseIndex}>{index + 1}</Text>
+            <View style={styles.expensedetail}>
+                <Text style={styles.expenseTitle}>{item.title}</Text>
+                <Text style={styles.expenseCategory}>{item.category}</Text>
+            </View>
+            <Text style={styles.expenseAmount}>RS {item.amount}</Text>
+        </View>
+    );
+};
+
+export default function Home({ navigation }) {
+
+    return (
+        <View style={styles.home}>
+            <ScrollView>
+                <View style={styles.highlight}>
+                    <View style={styles.expenditureView}>
+                        <Text style={styles.expenditureText}>Month's Expenditure</Text>
+                        <Text style={styles.expenditureNumber}>10,000</Text>
+                    </View>
+                    <View style={styles.highlights}>
+                        <View style={styles.highlightInfo}>
+                            <Text style={styles.highlightText}>Something</Text>
+                            <Text style={styles.highlightNumber}>10,000</Text>
+                        </View>
+                        <View style={styles.highlightInfo}>
+                            <Text style={styles.highlightText}>Something</Text>
+                            <Text style={styles.highlightNumber}>10,000</Text>
+                        </View>
+                        <View style={styles.highlightInfo}>
+                            <Text style={styles.highlightText}>Something</Text>
+                            <Text style={styles.highlightNumber}>10,000</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.expenditureDetailView}>
+                    <Text style={styles.expenditureDetailHead}>Recent Expenses</Text>
+                    <View style={styles.expenseList}>
+                        <FlatList data={DATA} renderItem={Item} keyExtractor={(item) => item.id} scrollEnabled={false} />
+                    </View>
+                    <Pressable style={styles.viewExpenses} onPress={() => navigation.navigate('Expenses')} >
+                        <Text style={styles.viewExpensesText}>View All</Text>
+                    </Pressable>
+                </View>
+                <View style={styles.expenditureDetailView}>
+                    <Text style={styles.expenditureDetailHead}>Recent Expenses</Text>
+                    <View style={styles.expenseList}>
+                        <FlatList data={DATA} renderItem={Item} keyExtractor={(item) => item.id} scrollEnabled={false} />
+                    </View>
+                    <Pressable style={styles.viewExpenses} onPress={() => navigation.navigate('Expenses')} >
+                        <Text style={styles.viewExpensesText}>View All</Text>
+                    </Pressable>
+                </View>
+            </ScrollView>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    home: {
+        flex: 1,
+    },
+    highlight: {
+        // height: "35%",
+        backgroundColor: '#e580ff',
+        borderRadius: 25,
+        margin: 10,
+        elevation: 5,
+    },
+    expenditureView: {
+        marginTop: 30,
+        // flex: 1,
+        alignItems: "center"
+    },
+    expenditureNumber: {
+        color: "white",
+        fontWeight: "800",
+        fontSize: 30
+    },
+    expenditureText: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 12,
+        letterSpacing: 0.8
+    },
+    highlights: {
+        // flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        marginVertical: 20
+    },
+    highlightInfo: {
+
+    },
+    highlightText: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 12,
+        letterSpacing: 0.8
+    },
+    highlightNumber: {
+        color: "white",
+        fontWeight: "800",
+        fontSize: 23
+    },
+    expenditureDetailView: {
+        // backgroundColor: "#c8d0d0",
+        backgroundColor: "white",
+        borderRadius: 25,
+        padding: 20,
+        margin: 10,
+        elevation: 5
+    },
+    expenditureDetailHead: {
+        fontWeight: "bold",
+        fontSize: 15,
+        letterSpacing: 0.8
+    },
+    viewExpenses: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        backgroundColor: '#e580ff',
+        borderRadius: 5,
+        elevation: 3,
+    },
+    viewExpensesText: {
+        color: "white"
+    },
+    expenseList: {
+        marginVertical: 10
+    },
+    expenseItem: {
+        backgroundColor: "white",
+        // flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        marginVertical: 5
+    },
+    expenseIndex: {
+        marginRight: 10
+    },
+    expensedetail: {
+        flex: 1,
+    },
+    expenseTitle: {
+        // fontWeight: "bold",
+        fontSize: 13,
+        letterSpacing: 0.25
+    },
+    expenseCategory: {
+        fontWeight: "bold",
+        fontSize: 10,
+        letterSpacing: 0.25
+    },
+    expenseAmount: {
+        fontWeight: "bold",
+        fontSize: 15,
+    }
+});
